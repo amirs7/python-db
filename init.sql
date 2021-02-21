e79518b266f07a98aaa91089f43b1dfd6d349e6a
Amir <ahsprim@gmail.com>
2021-02-22 01:23:28 +0330

    remove db init sql

--- init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE book (
-  id bigint(20) NOT NULL,
-  name varchar(25) DEFAULT NULL,
-  author varchar(25) NOT NULL,
-  PRIMARY KEY (id)
-);
-
-INSERT INTO book VALUES(1, 'book1', 'author1');
